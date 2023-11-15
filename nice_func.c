7d878ad7e778a03018be471d049589a3d38fd169
Alaa <lolomuhammd21@gmail.com>
2023-11-15 18:26:17 -0500

    4 

--- nice_func.c
+++ nice_func.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * cust_print_sn - this func to print the sn
+ * helping_print_sn - this func to print the sn
  * @word: point look the char str
  * @space: this is the size
  * @sun: look for const char
@@ -11,42 +11,42 @@
 
 void helping_print_sn(char *word, size_t space, const char *sun, ...)
 {
-    const char *ptints = sun;
-    char *book = word;
-    int values = 0;
-    size_t saveing = space;
-    va_list args;
+const char *ptints = sun;
+char *book = word;
+int values = 0;
+size_t saveing = space;
+va_list args;
 
-    while (*ptints != '\0' && saveing > 1)
-    {
-        if (*ptints == '%' && *(ptints + 1) == 's')
-        {
-            char *arg = va_arg(args, char *);
-            size_t arg_len = cust_from_len(arg);
+while (*ptints != '\0' && saveing > 1)
+{
+if (*ptints == '%' && *(ptints + 1) == 's')
+{
+char *arg = va_arg(args, char *);
+size_t arg_len = cust_from_len(arg);
 
-            if (arg_len < saveing - 1)
-            {
-                cust_to_cyp(book, arg);
-                book += arg_len;
-                saveing -= arg_len;
-                values += arg_len;
-            }
-            else
-            {
-                break;
-            }
+if (arg_len < saveing - 1)
+{
+cust_to_cyp(book, arg);
+book += arg_len;
+saveing -= arg_len;
+values += arg_len;
+}
+else
+{
+break;
+}
 
-            ptints += 2;
-        }
-        else
-        {
-            *book = *ptints;
-            book++;
-            ptints++;
-            saveing--;
-            values++;
-        }
-    }
+ptints += 2;
+}
+else
+{
+*book = *ptints;
+book++;
+ptints++;
+saveing--;
+values++;
+}
+}
 }
 /**
  * cust_print_sn - this func to print the sn
@@ -59,29 +59,29 @@ void helping_print_sn(char *word, size_t space, const char *sun, ...)
 
 int cust_print_sn(char *word, size_t space, const char *sun, ...)
 {
-    char *book = word;
-    int values = 0;
-    size_t saveing = space;
-    va_list args;
+char *book = word;
+int values = 0;
+size_t saveing = space;
+va_list args;
 
-    va_start(args, sun);
+va_start(args, sun);
 
-    if (word == NULL || sun == NULL || space == 0)
-    {
-        return (-1);
-    }
+if (word == NULL || sun == NULL || space == 0)
+{
+return (-1);
+}
 
 helping_print_sn(word, space, sun);
 
-    va_end(args);
+va_end(args);
 
-    if (saveing > 0)
-    {
-        *book = '\0';
-    }
-    else
-    {
-        return (-1);
-    }
-    return (values);
+if (saveing > 0)
+{
+*book = '\0';
+}
+else
+{
+return (-1);
+}
+return (values);
 }
